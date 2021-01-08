98deb8d5b4ccd184316112cf6fed4baa18828837
Harerimana Egide <egideharerimana085@gmail.com>
2021-01-08 07:42:13 +0200

    modified

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 "use strict";
 
-require('../mongodb/index')
+require('./mongodb/index')
 const express = require("express");
 const client_server = express();
-const serverLess = require("serverless-http")
 const cors = require("cors");
-const UserRouter = require("./user");
-const ChannelRouter = require("./channel");
-const MessagesRouter = require("./messages")
-const WorkspaceRouter = require("./workspace");
-const path = require("path");
+const UserRouter = require("./routers/user");
+const ChannelRouter = require("./routers/channel");
+const MessagesRouter = require("./routers/messages")
+const WorkspaceRouter = require("./routers/workspace");
 const bodyParser = require('body-parser');
-const api = require("./api")
 const upload = require("multer")({
     dest: "../public/images"
 });
 const cors_opts = {}
 const history = require("connect-history-api-fallback");
 const passport = require("passport");
-const router = express.Router()
 
 
 
@@ -40,23 +36,6 @@ client_server.get("/",(req,res)=>{
         message: "Server is already up & running."
     })
 })
-
-// ===============For netlify-lambda================
-router.use("/user", UserRouter);
-router.use("/channel", ChannelRouter);
-router.use("/workspace", WorkspaceRouter);
-router.use("/message", MessagesRouter);
-router.get("/",(req,res)=>{
-    return res.send({
-        status: "Working",
-        message: "Server is already up & running."
-    })
-})
-
-
-
-client_server.use("/.netlify/functions/index", router)
-client_server.use("/.netlify/functions/api",api)
 client_server.use(history());
 
 
@@ -66,8 +45,3 @@ const CLIENT_SERVER_PORT = process.env.PORT || 3000;
 client_server.listen(CLIENT_SERVER_PORT, function() {
     console.log(`#CLIENT_SERVER is listening on port: ${CLIENT_SERVER_PORT}`);
 });
-
-
-//========================FOR NETLIFY==================
-module.exports = client_server
-module.exports.handler = serverLess(client_server)
--- routers/api.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router()
-
-router.get("/",(req,res)=>{
-    return res.send("Hello, this is api test0")
-})
-
-module.exports = router

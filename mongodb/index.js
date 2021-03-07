1ab5f44b76f0106c37316ce12c4100caab02cd28
Harerimana Egide <egideharerimana085@gmail.com>
2021-03-07 21:47:41 +0200

    modified

--- mongodb/index.js
+++ mongodb/index.js
@@ -1,26 +1,22 @@
 const mongoose = require('mongoose')
-const bcrypt = require('bcryptjs');
 
 // production! mongodb atlas
-const connection = mongoose.connect('mongodb+srv://egide:8QVEpKfW8r2UVPsw@devdbs.xcnlk.gcp.mongodb.net/teamDb?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then((conn) => {
-    console.log("#Connected to mongodb successfully...")
-    return conn
-}).catch((err) => {
-    console.log("#Failed to connect to mongodb:", err.message)
-})
-
-// development! localhost mongodb
-// const connection = mongoose.connect('mongodb://localhost:27017/teamDb', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// }).then(()=>{
+// const connection = mongoose.connect('mongodb+srv://egide:8QVEpKfW8r2UVPsw@devdbs.xcnlk.gcp.mongodb.net/teamDb?retryWrites=true&w=majority', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// }).then((conn) => {
 //     console.log("#Connected to mongodb successfully...")
-// }).catch(()=>{
-//     console.log("#Failed to connect to mongodb...")
+//     return conn
+// }).catch((err) => {
+//     console.log("#Failed to connect to mongodb:", err.message)
 // })
 
-// console.log(bcrypt.hashSync("123456",2))
-// console.log(Date())
+// development! localhost mongodb
+const connection = mongoose.connect('mongodb://localhost:27017/teamDb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(()=>{
+    console.log("#Connected to mongodb successfully...")
+}).catch(()=>{
+    console.log("#Failed to connect to mongodb...")
+})
